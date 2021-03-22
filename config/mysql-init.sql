e28812e28c9d4939116dc156981a5d7ed59acf43
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-03-22 15:08:55 +0200

    rename localhost

--- config/mysql-init.sql
+++ config/mysql-init.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS hedera_01;
 CREATE DATABASE IF NOT EXISTS hedera_02;
 CREATE DATABASE IF NOT EXISTS hedera_03;
 
-CREATE USER 'hedera_01'@'localhost' IDENTIFIED BY 'hedera_pass';
-GRANT ALL PRIVILEGES ON hedera_01.* TO 'hedera_01'@'localhost';
+CREATE USER 'hedera_01'@'%' IDENTIFIED BY 'hedera_pass';
+GRANT ALL PRIVILEGES ON hedera_01.* TO 'hedera_01'@'%';
 FLUSH PRIVILEGES;
-CREATE USER 'hedera_02'@'localhost' IDENTIFIED BY 'hedera_pass';
-GRANT ALL PRIVILEGES ON hedera_02.* TO 'hedera_02'@'localhost';
+CREATE USER 'hedera_02'@'%' IDENTIFIED BY 'hedera_pass';
+GRANT ALL PRIVILEGES ON hedera_02.* TO 'hedera_02'@'%';
 FLUSH PRIVILEGES;
-CREATE USER 'hedera_03'@'localhost' IDENTIFIED BY 'hedera_pass';
-GRANT ALL PRIVILEGES ON hedera_03.* TO 'hedera_03'@'localhost';
+CREATE USER 'hedera_03'@'%' IDENTIFIED BY 'hedera_pass';
+GRANT ALL PRIVILEGES ON hedera_03.* TO 'hedera_03'@'%';
 FLUSH PRIVILEGES;
