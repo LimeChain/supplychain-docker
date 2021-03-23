7d8632f10e43630e2774fea6c37b57ff4375ee32
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-03-23 19:30:03 +0200

    instances

--- config/mongo-init.js
+++ config/mongo-init.js
@@ -1,1 +1,8 @@
-db.createCollection('hcs-integration-node');
+db01 = db.getSiblingDB('hcs-integration-node-01');
+db01.createCollection('hcs-integration-node-01');
+
+db02 = db.getSiblingDB('hcs-integration-node-02');
+db02.createCollection('hcs-integration-node-02');
+
+db03 = db.getSiblingDB('hcs-integration-node-03');
+db03.createCollection('hcs-integration-node-03');
