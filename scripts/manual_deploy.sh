dcfc78a54a1bc685ef80fd79547e1b176f21e9fe
Kaloyan Dobrev <k.dobrev@razorlabs.com>
2021-03-24 11:11:48 +0200

    Deployment Nodes

--- scripts/manual_deploy.sh
+++ scripts/manual_deploy.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-BUILD_VERSION="v23"
+BUILD_VERSION="v27"
 
 gcloud config configurations activate pwc-bat-test
 export CLOUDSDK_ACTIVE_CONFIG_NAME=pwc-bat-test
@@ -18,8 +18,11 @@ docker push "gcr.io/pwc-bat-test/hedera-web-service-03:${BUILD_VERSION}"
 
 cd ../HederaIntegrationNode
 
-docker build -f ./Dockerfile -t gcr.io/pwc-bat-test/hedera-integration-node:${BUILD_VERSION} .
-docker push "gcr.io/pwc-bat-test/hedera-integration-node:${BUILD_VERSION}"
+docker build -f ./Dockerfile.01 -t gcr.io/pwc-bat-test/hedera-integration-node-01:${BUILD_VERSION} .
+docker push "gcr.io/pwc-bat-test/hedera-integration-node-01:${BUILD_VERSION}"
+
+docker build -f ./Dockerfile.02 -t gcr.io/pwc-bat-test/hedera-integration-node-02:${BUILD_VERSION} .
+docker push "gcr.io/pwc-bat-test/hedera-integration-node-02:${BUILD_VERSION}"
 
 cd ../pwc-bat-docker
 kubectl apply -f kubernetes
